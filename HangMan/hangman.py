cdd506d6cf3ba3feda4dc22f32790a9ad610afc6
George <Joel.George@commscope.com>
2022-06-23 11:47:53 -0500

    Started coding the hangman game

--- HangMan/hangman.py
+++ HangMan/hangman.py
@@ -1,3 +1,35 @@
 from words import words
+import random
+import string
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in words or " " in words:
+        word = random.choice(words)
+    return word.lower()
+
+
+def hangman():
+    random_word = get_valid_word(words)
+    print(random_word)
+
+    letters_in_word = set(random_word)
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set()
+    user_letter = input("Enter a string: ")
+    print(letters_in_word)
+
+    if user_letter.lower() in alphabet - used_letters:
+        print("checking guess")
+        used_letters.add(user_letter)
+        if user_letter in letters_in_word:
+            letters_in_word.remove(user_letter)
+            
+    elif user_letter.lower() in used_letters:
+        print("The letter has already been guesssed")
+    else:
+        print("Invalid entry")
+
+
+
+hangman()
