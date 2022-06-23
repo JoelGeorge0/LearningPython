d6107494f3f6c8d606f269bfd8b5bbc3cc87d983
George <Joel.George@commscope.com>
2022-06-23 14:23:27 -0500

    finished the hangman program

--- HangMan/hangman.py
+++ HangMan/hangman.py
@@ -16,20 +16,27 @@ def hangman():
     letters_in_word = set(random_word)
     alphabet = set(string.ascii_lowercase)
     used_letters = set()
-    user_letter = input("Enter a string: ")
-    print(letters_in_word)
 
-    if user_letter.lower() in alphabet - used_letters:
-        print("checking guess")
-        used_letters.add(user_letter)
-        if user_letter in letters_in_word:
-            letters_in_word.remove(user_letter)
-            
-    elif user_letter.lower() in used_letters:
-        print("The letter has already been guesssed")
-    else:
-        print("Invalid entry")
 
+    while len(letters_in_word)>0:
+        print("You have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "-" for letter in random_word]
+
+        print("current word: ", " ".join(word_list))
+
+        user_letter = input("Enter a string: ").lower()
+
+        if user_letter.lower() in alphabet - used_letters:
+            used_letters.add(user_letter.lower())
+            if user_letter.lower() in letters_in_word:
+                letters_in_word.remove(user_letter.lower())   
+        elif user_letter.lower() in used_letters:
+            print("The letter has already been guesssed")
+        else:
+            print("Invalid entry")
+
+    print("congrants you guess correclty, the word was: " + random_word)
 
 
 hangman()
